30eaa3131f9804d7c06bdbc08667629db3520f3b
Rumana <rumanaparven16@gmail.com>
2020-10-25 10:46:18 +0530

    first commit

---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        static void Main(string[] args)
+        {
+  
+                hash.Add("0", "To");
+               // hash.Add("1", "be");
+                hash.Add("2", "or");
+                //hash.Add("3", "not");
+               //hash.Add("4", "to");
+                //hash.Add("5", "be");
+
+                string second = hash.Get("2");
+                Console.WriteLine("2nd index value :" + second);
+                hash.Remove("1");
+
+            
+        }
+    }
+}
